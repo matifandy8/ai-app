856c68d51e3d6b478551b3f1082b57fc586721bf
matias <matifandy@gmail.com>
2023-04-06 14:48:07 -0300

    feat: send answer

--- src/app/playground/interview/page.tsx
+++ src/app/playground/interview/page.tsx
@@ -4,10 +4,21 @@ import { fetchQuestionMessage } from '@/app/utils/fetchquestions'
 import { ChangeEventHandler, useState } from 'react'
 import './styles/Interview.scss'
 
+type Answer = {
+    id: number
+    answer: string
+}
+
 export default function Interview() {
     const [technology, setTechnology] = useState('')
     const [seniority, setSeniority] = useState('')
     const [questions, setQuestions] = useState('')
+    const [answer, setAnswer] = useState('')
+    const [answers, setAnswers] = useState<Answer[]>([])
+
+    const addAnswer = (answer: string) => {
+        setAnswers([...answers, { id: answers.length + 1, answer }])
+    }
 
     const handleTechnologyChange: ChangeEventHandler<HTMLSelectElement> = e => {
         setTechnology(e.target.value)
@@ -16,14 +27,28 @@ export default function Interview() {
     const handleSeniorityChange: ChangeEventHandler<HTMLSelectElement> = e => {
         setSeniority(e.target.value)
     }
+    const handleAnswerChange: ChangeEventHandler<HTMLInputElement> = e => {
+        setAnswer(e.target.value)
+    }
 
     const handleSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault()
+        // try {
+        //     const message = await fetchQuestionMessage(technology, seniority);
+        //     setCurrentQuestion(message);
+        //   } catch (error) {
+        //     console.error(error);
+        //     // Display error message to user
+        //   }
         // const message = await fetchQuestionMessage(technology, seniority);
         // setQuestions(message);
         setQuestions('hi, this is a test question is not fetching any data?')
     }
-
+    function handleClick() {
+        if (!answer) return
+        addAnswer(answer)
+        setAnswer('')
+    }
     return (
         <div className="interview">
             <h1 className="interview__title">Receive Personalized Interview</h1>
@@ -66,8 +91,26 @@ export default function Interview() {
                     </p>
                 ) : (
                     <div className="interview__chat">
-                        <div className="interview__question">{questions}</div>
-                        <input type="text" />
+                        <div className="interview__question-msg">
+                            {questions}
+                        </div>
+                        {answers?.map(answer => (
+                            <div
+                                className="interview__answer-msg"
+                                key={answer.id}
+                            >
+                                {answer.answer}
+                            </div>
+                        ))}
+                        <div className="interview__answer">
+                            <input
+                                type="text"
+                                placeholder="Enter your answer..."
+                                value={answer}
+                                onChange={handleAnswerChange}
+                            />
+                            <button onClick={handleClick}>Send</button>
+                        </div>
                     </div>
                 )}
             </div>
